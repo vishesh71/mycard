c8cce95dd5ee080a42b3f4c80865a9d3d002e0c4
Vishesh Kumar <vk7879@srmist.edu.in>
2022-01-19 00:03:43 +0530

    final app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,35 +12,99 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.orangeAccent,
+        backgroundColor: Colors.orange[200],
         body: SafeArea(
-            child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundColor: Colors.deepOrange,
-              backgroundImage: AssetImage('images/pm.png'),
+            child: Center(
+          child: Container(
+            //color: Colors.green,
+            decoration: BoxDecoration(
+                color: Colors.orange, borderRadius: BorderRadius.circular(15)),
+            child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 55,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage('images/pm.png'),
+                      backgroundColor: Colors.deepOrange,
+                    ),
+                  ),
+                  Text(
+                    'Jai Shree Ram',
+                    style: GoogleFonts.pacifico(
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text('Narendra Modi',
+                      style: GoogleFonts.staatliches(
+                          fontSize: 30,
+                          color: Colors.white60,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.orange[100],
+                        borderRadius: BorderRadius.circular((5))),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                      child: Row(
+                        //crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.add_call),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            '9876543210',
+                            style: GoogleFonts.aleo(
+                                fontSize: 20, color: Colors.red),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.orange[100],
+                        borderRadius: BorderRadius.circular((5))),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                      child: Row(
+                        //crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.email),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'modiji@bharat.jaihind',
+                            style: GoogleFonts.aleo(
+                                fontSize: 20, color: Colors.red),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              'Jai Shree Ram',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                  fontSize: 40,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text('Narendra Damodardas Modi',
-            style: TextStyle(
-              fontFamily: 'AmaticSC',
-              fontSize: 30,
-                color: Colors.white60,
-              fontWeight: FontWeight.bold)
-            )
-
-
-          ],
-
+          ),
         )),
       ),
     );
